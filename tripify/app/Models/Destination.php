56dd63662a66891d7d951c2a66a096bdc15e3d7f
Bruno Benčević <bencevic.bruno@gmail.com>
2023-07-04 20:13:15 +0200

    yes

--- tripify/app/Models/Destination.php
+++ tripify/app/Models/Destination.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Destination extends Model
 {
     use HasFactory;
-    protected $table = 'destination';
+    protected $table = 'destinations';
 }
---
+++ tripify/resources/views/auth/register_user.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ URL::asset('css/register.css') }}" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;600;900&display=swap" rel="stylesheet">
+</head>
+<body>
+    
+<x-guest-layout>
+    <x-authentication-card>
+
+        <div class="container register-container w-25">
+            <form class="form" method="POST" action="{{ route('register') }}">
+                @csrf
+
+                <div>
+                    <label for="name" value="name">Name</label>
+                    <br>
+                    <input class="rounded" id="name" type="text" name="name" :value="old('name')" required autocomplete="OFF" />
+                </div>
+
+                <div class="mt-3">
+                    <label for="name" value="surname">Surname</label>
+                    <br>
+                    <input class="rounded" id="surname" type="text" name="surname" :value="old('surname')" required autocomplete="OFF" />
+                </div>
+
+                <div class="mt-3">
+                    <label for="email" value="email">Email</label>
+                    <br>
+                    <input class="rounded" id="email" type="email" name="email" :value="old('email')" required autocomplete="OFF" />
+                </div>
+
+                <div class="mt-3">
+                    <label for="password" value="password">Password</label>
+                    <br>
+                    <input class="rounded" id="password" type="password" name="password" required autocomplete="OFF" />
+                </div>
+
+                <div class="mt-3">
+                    <label for="password_confirmation" value="password_confirmation">Confirm Password</label>
+                    <br>
+                    <input class="rounded" id="password_confirmation" type="password" name="password_confirmation" required autocomplete="OFF" />
+                </div>
+
+                @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
+                    <div class="mt-4">
+                        <x-label for="terms">
+                            <div class="flex items-center">
+                                <x-checkbox name="terms" id="terms" required />
+
+                                <div class="ml-2">
+                                    {!! __('I agree to the :terms_of_service and :privacy_policy', [
+                                            'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Terms of Service').'</a>',
+                                            'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Privacy Policy').'</a>',
+                                    ]) !!}
+                                </div>
+                            </div>
+                        </x-label>
+                    </div>
+                @endif
+
+                <button class="register-button mt-3">
+                    {{ __('Register') }}
+                <button>                        
+
+                <div class="already-registered-container text-center mt-3">
+                    <a href="{{ route('login') }}">
+                        {{ __('Already registered?') }}
+                    </a>
+                </div>
+
+            </form>
+        </div>
+
+    </x-authentication-card>
+</x-guest-layout>
+
+</body>
+</html>
